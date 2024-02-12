76d416e4db91c1b9d6249d3125590eb80fbbdeb3
AKhil Nayak <aakhiljay06@gmail.com>
2024-02-13 01:16:27 +0530

    added database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const app = require("./app");
+require("./db")
 const PORT = 5000;
 
 let server = app.listen(PORT , (err) =>{
     if(!err){
-        console.log("Server Listening Successfully...")
+        console.log("Server Listening Sucacessfully...")
     }
     
 })
